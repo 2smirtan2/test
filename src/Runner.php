d06632abdfddbe668142ea7719147b137bb70bf2
2smirtan2 <tan.kov@hotmail.com>
2021-05-10 00:43:23 +0300

    add Runner.php

---
+++ src/Runner.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Example\Php\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])-> map(function ($name) {
+        return strtoupper($$name);
+    });
+    return $collection;
+}
